168de4e3f2254b307c589a8f7cc1aade739741cd
Arman Siddiqui <arman2113022@akgec.ac.in>
2024-02-20 20:51:21 +0530

    Added Routes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import store from './store';
 import HomePage from './Component/HomePage';
-// import Login from './Component/Login'
+import Login from './Component/Login';
 
 class App extends Component {
-  
   render() {
     return (
       <Provider store={store}>
-        <HomePage />
+        <Router>
+          <Routes>
+            <Route exact path='/' element={<HomePage />} />
+            <Route exact path='/login' element={<Login />} />
+          </Routes>
+        </Router>
       </Provider>
     );
   }
